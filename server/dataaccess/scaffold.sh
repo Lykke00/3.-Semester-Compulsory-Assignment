aec9b2b2aa0fae2dabe891279ad0c4a7caa83ab0
Lykke <lykber01@easv365.dk>
2025-09-17 14:44:35 +0200

    scaffold script changed + scaffolded database

--- server/dataaccess/scaffold.sh
+++ server/dataaccess/scaffold.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#The below lines are to get the CONN_STR="the .net formatted connection string" from the .env file in this directory (gitignored).
-#You can also simply put the secret straight into the scaffold command and remember not to commit it.
+# Load connection string from .env
 set -a
 source .env
 set +a
 
-dotnet tool install -g dotnet-ef && dotnet ef dbcontext scaffold "$CONN_STR" Npgsql.EntityFrameworkCore.PostgreSQL   --context MyDbContext     --no-onconfiguring        --schema library   --force
+dotnet tool install -g dotnet-ef && dotnet ef dbcontext scaffold "$CONN_STR" Npgsql.EntityFrameworkCore.PostgreSQL \
+  --context MyDbContext \
+  --no-onconfiguring \
+  --schema library \
+  --output-dir Models \
+  --force
