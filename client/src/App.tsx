d4bb96398af699e540fa1cf468c55d89f4d406c4
Lykke <lykber01@easv365.dk>
2025-09-29 10:09:02 +0200

    added darkmode styling and navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,26 @@
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import './App.css'
 import HomePage from './pages/Home'
 import BooksPage from './pages/Books'
 import AuthorsPage from './pages/Authors'
 import GenresPage from './pages/Genres'
+import Layout from './components/layout'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "books", element: <BooksPage /> },
+        { path: "authors", element: <AuthorsPage /> },
+        { path: "genres", element: <GenresPage /> },
+      ],
+    },
+  ]);
 
-  return (
-        <RouterProvider router={createBrowserRouter([
-            {
-                path: '',
-                element: <HomePage/>,
-                children: [
-                    {
-                        path: 'books',
-                        element: <BooksPage/>
-                    },
-                    {
-                        path: 'authors',
-                        element: <AuthorsPage/>
-                    },
-                    {
-                        path: 'genres',
-                        element: <GenresPage/>
-                    }
-                ]
-            }
-        ])}/>
+  return <RouterProvider router={router} />;
 
-  )
 }
 
 export default App
---
+++ client/src/components/layout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router";
+import { Navbar } from "./navigation/Navbar";
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-zinc-950">
+      <div className="w-full border-b flex justify-center p-2 bg-zinc-900 border-zinc-700">
+        <Navbar />
+      </div>
+
+      <main className="flex-1 flex items-center justify-center p-6">
+        <div className="w-full max-w-4xl">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
